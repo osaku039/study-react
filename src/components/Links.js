ac6200526340c8ecdd8258c97396d52f94577129
osaku039 <booklove039@gmail.com>
2025-01-20 02:27:09 +0900

    見た目の調整

--- src/components/Links.js
+++ src/components/Links.js
@@ -17,27 +17,17 @@ export function Links() {
   return (
           <div className={styles.ctas}>
             <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+              href="/gourmet"
+              className={styles.secondary}
             >
               <Image
                 className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
+                src="/window.svg"
+                alt="logomark"
                 width={20}
                 height={20}
               />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
+              お店を検索
             </a>
           </div>
   );
