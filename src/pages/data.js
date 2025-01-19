ecf431d1c90a8ff9da721ee8f96a3495f6005abc
osaku039 <booklove039@gmail.com>
2025-01-19 21:28:36 +0900

    defaultにする

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,6 +1,6 @@
 "use server";
 
-export async function fetchGourmet() {
+export default async function fetchGourmet() {
   const place = "Tokyo";
   const url = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=";
   const api_url = `${url}${process.env.NEXT_PUBLIC_API_KEY}&large_area=Z011&format=json`;
